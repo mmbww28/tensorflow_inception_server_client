8b56b86c889a62891bb95954edde7bdd45a664d8
kelly fritz <hsaoaqvpzy@gmail.com>
2018-12-10 17:30:21 +0900

    revision 2

--- get_feature.py
+++ get_feature.py
@@ -1,14 +1,9 @@
 import iv3_client
 from PIL import Image
-from mem_top import mem_top
 
 counter=0
 img=Image.open('lenna.jpg')
 while True:
   result=iv3_client.run([img])
-  #print(str(len(result[0]))+'  '+str(result[0][:3])+'...')
-  if counter%100==0:
-    print('mem_top: '+str(mem_top()))
-    counter=0
-  counter+=1
+  print(str(len(result[0]))+'  '+str(result[0][:3])+'...')
 
